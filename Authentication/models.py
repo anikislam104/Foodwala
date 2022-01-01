15d47e4f0fc2aca1f1534ed55ed24640b44b551c
Anik Islam Pantha <1705104@ugrad.cse.buet.ac.bd>
2022-01-02 03:38:27 +0600

    designed front end of sign up page

--- Authentication/models.py
+++ Authentication/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=200)
+    email=models.EmailField(max_length=200)
+    pwd=models.CharField(max_length=200)
+    userType=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
--- Authentication/views.py
+++ Authentication/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("I am Pantha")
+    return render(request,'Authentication/SignUp.html')
